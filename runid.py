ce5d4ace654ca131392aab395039519008844aaf
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2013-01-09 09:49:08 +0100

    Fix runid since .redo/bin not always created

--- runid.py
+++ runid.py
@@ -11,6 +11,8 @@ def read(filename):
 
 
 def change(filename):
+    try: os.makedirs(os.path.dirname(filename))
+    except: pass
     orig_runid = read(filename)
     runid = orig_runid + 1
     os.utime(filename, (runid, runid))
