3973adb711173d2b7a118c32f7cec8f78ec249f2
Mildred Ki'Lya <mildred-pub.git@mildred.fr>
2013-02-17 13:19:15 +0100

    Use real timestamps for RUNID

--- runid.py
+++ runid.py
@@ -2,18 +2,25 @@ import os
 
 
 def read(filename):
-    try:
-        return int(os.stat(filename).st_mtime)
-    except OSError:
-        open(filename, 'a')
-        os.utime(filename, (1,1))
-        return 1
+    "read filename mtime (create file if it doesn't exists)"
+    with open(filename, 'a') as f:
+        return int(os.fstat(f.fileno()).st_mtime)
 
 
 def change(filename):
+    """Increment mtime from filename, using the current time if possible,
+    creating the file if it doesn't exists"""
     try: os.makedirs(os.path.dirname(filename))
     except: pass
-    orig_runid = read(filename)
-    runid = orig_runid + 1
-    os.utime(filename, (runid, runid))
-    return runid
+    try:
+        mtime1 = int(os.stat(filename).st_mtime)
+    except OSError:
+        mtime1 = 0
+    with open(filename, 'a') as f:
+        os.utime(filename, None)
+        mtime2 = int(os.fstat(f.fileno()).st_mtime)
+        if mtime2 <= mtime1:
+            mtime2 = mtime1 + 1
+            os.utime(filename, (mtime2, mtime2))
+        return mtime2
+
